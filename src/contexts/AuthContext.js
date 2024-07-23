14dc864399e5065110cd9180bc1e6a3c7591267b
DiegoRojo99 <diego99rojo@gmail.com>
2024-07-23 23:49:35 +0100

    Added AuthContext

---
+++ src/contexts/AuthContext.js
@@ -0,0 +1,32 @@
+import React, { createContext, useContext, useEffect, useState } from 'react';
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from '../Firebase';
+
+const AuthContext = createContext();
+
+export function AuthProvider({ children }) {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setUser(user);
+      } else {
+        setUser(null);
+      }
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ user }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+// Custom hook to use the auth context
+export function useAuth() {
+  return useContext(AuthContext);
+}

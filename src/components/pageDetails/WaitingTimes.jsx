af0105300c6541e9b5c3452405a99435f6f2a686
DiegoRojo99 <diego99rojo@gmail.com>
2024-07-23 19:58:30 +0100

    Sorting wait times

--- src/components/pageDetails/WaitingTimes.jsx
+++ src/components/pageDetails/WaitingTimes.jsx
@@ -7,23 +7,54 @@ export function WaitingTimes({ type, attractions }){
     return <p>Loading...</p>;
   }
 
+  function sortByWaitTime(a, b){
+    const aWaitTime = a.queue?.STANDBY?.waitTime;
+    const bWaitTime = b.queue?.STANDBY?.waitTime;
+    if(!aWaitTime && bWaitTime){
+      return 1;
+    }
+    else if (!bWaitTime && aWaitTime){
+      return -1;
+    }
+    else if(!aWaitTime && !bWaitTime){
+      if(a.status === b.status){
+        return 0;
+      }
+      else if(a.status === "CLOSED"){
+        return 1;
+      }
+      else if(a.status === "OPERATING"){
+        return -1;
+      }
+      else if(b.status === "CLOSED"){
+        return -1;
+      }
+      else if(b.status === "OPERATING"){
+        return 1;
+      }
+    }
+    else{
+      return bWaitTime - aWaitTime;
+    }
+  }
+
   return (
     <div>               
       <div className='waiting-times'>
         <div className='attraction-row header-row'>
-            <p>Attraction</p>
-            <p>Waiting Time</p>
-            <p>Status</p>
+          <p>Attraction</p>
+          <p>Waiting Time</p>
+          <p>Status</p>
         </div>
-      {attractions.map((a) => {
-        return (
-          <div className='attraction-row'>
-            <p>{a.name}</p>
-            <p>{a.queue && a.queue["STANDBY"] && a.queue["STANDBY"].waitTime ? a.queue["STANDBY"].waitTime : "-"}</p>
-            <p>{a.status}</p>
-          </div>
-        )
-      })}
+        { attractions.sort((a,b) => sortByWaitTime(a, b)).map((a) => {
+          return (
+            <div className='attraction-row'>
+              <p>{a.name}</p>
+              <p>{a.queue?.STANDBY?.waitTime ? a.queue["STANDBY"].waitTime : "-"}</p>
+              <p>{a.status}</p>
+            </div>
+          )
+        })}
       </div>
     </div>
   );

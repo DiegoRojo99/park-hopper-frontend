59a81fc66558b2e804a5ce2422878aca6d6f6755
DiegoRojo99 <diego99rojo@gmail.com>
2024-08-19 23:36:08 +0100

    Updated nav

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,20 +1,27 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
-import './Nav.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
-import { useState } from 'react';
+import './Nav.css';
 
 function Nav() {
   const [openMenu, setOpenMenu] = useState(false);
+
+  function handleLinkClick(){
+    setOpenMenu(false);
+  }
+
   return (
-    <div className={openMenu ? "nav-menu responsive" : "nav-menu"}>
-      <Link className='nav-logo' to='/'>Park Hopper</Link>
-      <Link to="/login">Login/Logout</Link>
-      <Link to="/settings">Settings</Link>
-      <Link to="/favs">Favs</Link>
-      <Link to="/explore">Explore</Link>
-      <a className='icon'>
-        <FontAwesomeIcon icon={faBars} onClick={() => setOpenMenu(!openMenu)} />
+    <div className="nav-menu">
+      <Link className='nav-logo' to='/'  onClick={handleLinkClick}>Park Hopper</Link>
+      <div className={openMenu ? "menu-items open" : "menu-items"}>
+        <Link to="/explore" onClick={handleLinkClick}>Explore</Link>
+        <Link to="/favs" onClick={handleLinkClick}>Favs</Link>
+        <Link to="/settings" onClick={handleLinkClick}>Settings</Link>
+        <Link to="/login" onClick={handleLinkClick}>Login/Logout</Link>     
+      </div>
+      <a className='icon' onClick={() => setOpenMenu(!openMenu)}>
+        <FontAwesomeIcon icon={faBars} />
       </a>
     </div>
   );

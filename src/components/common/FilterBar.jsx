924386cb14a5201a218fe7656963ac6efec21dc7
DiegoRojo99 <diego99rojo@gmail.com>
2024-08-22 22:23:54 +0100

    Reduced search bar height

--- src/components/common/FilterBar.jsx
+++ src/components/common/FilterBar.jsx
@@ -10,7 +10,15 @@ export default function FilterBar({name, searchName, placeholder}){
         value={name}
         onChange={(event) => { searchName(event.target.value)}}
         focused
-        sx={{ color: 'white', width: '100%' }}
+        sx={{ 
+          color: 'white', 
+          width: '100%',
+          fontSize: '1em' ,
+          '& .MuiInputBase-input': {
+            fontSize: '1em',
+            padding: '8px', // Adjust these values as needed
+          },
+        }}
       />
     </div>
   )

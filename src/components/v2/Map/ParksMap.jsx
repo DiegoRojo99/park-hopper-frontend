51280a971f6a1f25563cdd06b0191163a57124df
DiegoRojo99 <diego99rojo@gmail.com>
2024-08-25 13:47:42 +0100

    Map based on user position

--- src/components/v2/Map/ParksMap.jsx
+++ src/components/v2/Map/ParksMap.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 
@@ -9,6 +9,7 @@ import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
 import markerShadow from 'leaflet/dist/images/marker-shadow.png';
 
 import '../../common/Common.css';
+import { Loader } from '../../common/Loader';
 
 L.Icon.Default.mergeOptions({
   iconRetinaUrl: markerIcon2x,
@@ -17,10 +18,38 @@ L.Icon.Default.mergeOptions({
 });
 
 function ParksMap({ places, openLink }){
-  const defaultPosition = [51.505, -0.09]; // Default coordinates (e.g., London)
+
+  const [userPosition, setUserPosition] = useState(false);
+  const [zoom, setZoom] = useState(1);
+  const defaultPosition = [30, 0]; // Default coordinates (e.g., London)
+
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          setUserPosition([position.coords.latitude, position.coords.longitude]);
+          setZoom(14)
+        },
+        () => {
+          console.error("Unable to retrieve your location");
+          setUserPosition(defaultPosition);
+          setZoom(1);
+        }
+      );
+    } else {
+      console.error("Geolocation is not supported by your browser");
+      setUserPosition(defaultPosition);
+      setZoom(1);
+    }
+  }, []);
+
+
+  if (!userPosition) {
+    return <Loader />
+  }
 
   return (
-    <MapContainer center={defaultPosition} zoom={1} className='parks-map'>
+    <MapContainer center={userPosition} zoom={zoom} className='parks-map'>
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'

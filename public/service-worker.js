56c6804aaa210b43a02644729fce52b0e234fac3
DiegoRojo99 <diego99rojo@gmail.com>
2024-09-08 15:24:31 +0100

    Deleting alerts in the backend for now.
    Logs still there in case they log clicks

--- public/service-worker.js
+++ public/service-worker.js
@@ -9,8 +9,7 @@ self.addEventListener('push', function(event) {
     icon: data.icon || '/favicon.ico',
     badge: data.badge || '/favicon.ico',
     data: {
-      attractionId: data.attractionId,
-      userId: data.userId
+      attractionId: data.attractionId
     }
   };
 
@@ -18,48 +17,45 @@ self.addEventListener('push', function(event) {
 });
 
 self.addEventListener('notificationclick', function(event) {
-  sendLogToServer(`Notification clicked event triggered`);
   const attractionId = event?.notification?.data?.attractionId;
-  const userId = event?.notification?.data?.userId;
-  sendLogToServer(`Notification clicked. Attraction ID: ${attractionId}, User ID: ${userId}`);
   event.notification.close();
-
-  if (attractionId && userId) {
-    markNotificationAsSeen(attractionId, userId);
+  if (attractionId) {
+    sendLogToServer(`Notification clicked. Attraction ID: ${attractionId}`);
+    // clients.openWindow("https://parkhopper.live/");
   } else {
-    sendLogToServer('Missing attractionId or userId on missing');
+    sendLogToServer('Missing attractionId on click');
   }
 });
 
 self.addEventListener('notificationclose', function(event) {
-  const attractionId = event.notification.data.attractionId;
-  const userId = event.notification.data.userId;
-  if (attractionId && userId) {
-    markNotificationAsSeen(attractionId, userId);
+  const attractionId = event?.notification?.data?.attractionId;
+  if (attractionId) {
+    // markNotificationAsSeen(attractionId, userId);
+    sendLogToServer('Notification close triggered');
   } else {
-    sendLogToServer('Missing attractionId or userId on close');
+    sendLogToServer('Missing attractionId on close');
   }
 });
 
 // Function to mark the notification as seen
-async function markNotificationAsSeen(attractionId, userId) {
-  if (!attractionId || !userId) return;
+// async function markNotificationAsSeen(attractionId, userId) {
+//   if (!attractionId || !userId) return;
 
-  try {
-    const response = await fetch(`${apiUrl}/notifications/seen`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ attraction: attractionId, user: userId })
-    });
+//   try {
+//     const response = await fetch(`${apiUrl}/notifications/seen`, {
+//       method: 'POST',
+//       headers: {
+//         'Content-Type': 'application/json',
+//       },
+//       body: JSON.stringify({ attraction: attractionId, user: userId })
+//     });
 
-    if (response.ok) {
-      sendLogToServer(`Notification marked as seen. Attraction ID: ${attractionId}, User ID: ${userId}`);
-    } else {
-      sendLogToServer(`Failed to mark notification as seen: ${response.status}`);
-    }
-  } catch (error) {
-    sendLogToServer(`Failed to mark notification as seen: ${error}`);
-  }
-};
+//     if (response.ok) {
+//       sendLogToServer(`Notification marked as seen. Attraction ID: ${attractionId}, User ID: ${userId}`);
+//     } else {
+//       sendLogToServer(`Failed to mark notification as seen: ${response.status}`);
+//     }
+//   } catch (error) {
+//     sendLogToServer(`Failed to mark notification as seen: ${error}`);
+//   }
+// };

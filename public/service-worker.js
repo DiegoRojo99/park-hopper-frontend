e702c63241779cafbf20d253769ab50e81a304db
DiegoRojo99 <diego99rojo@gmail.com>
2024-09-08 14:38:28 +0100

    Updated service worker to include data in notification

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,13 +1,16 @@
-const apiUrl = process.env.REACT_APP_API_URL; 
+const apiUrl = process.env.REACT_APP_API_URL;
 
 self.addEventListener('push', function(event) {
-
   const data = event.data.json();
   const title = data.title || 'Default Title';
   const options = {
-      body: data.body || 'Default body',
-      icon: data.icon || '/favicon.ico',
-      badge: data.badge || '/favicon.ico',
+    body: data.body || 'Default body',
+    icon: data.icon || '/favicon.ico',
+    badge: data.badge || '/favicon.ico',
+    data: {
+      attractionId: data.attractionId,
+      userId: data.userId
+    }
   };
 
   event.waitUntil(self.registration.showNotification(title, options));
@@ -18,18 +21,21 @@ self.addEventListener('notificationclick', function(event) {
   const userId = event.notification.data.userId;
   event.notification.close();
 
-  // You can open the app or a specific URL if needed
-  // event.waitUntil(
-  //   clients.openWindow('/some-url')
-  // );
-
-  markNotificationAsSeen(attractionId, userId);
+  if (attractionId && userId) {
+    markNotificationAsSeen(attractionId, userId);
+  } else {
+    console.error('Attraction ID or User ID is missing');
+  }
 });
 
 self.addEventListener('notificationclose', function(event) {
   const attractionId = event.notification.data.attractionId;
   const userId = event.notification.data.userId;
-  markNotificationAsSeen(attractionId, userId);
+  if (attractionId && userId) {
+    markNotificationAsSeen(attractionId, userId);
+  } else {
+    console.error('Attraction ID or User ID is missing on close');
+  }
 });
 
 // Function to mark the notification as seen
@@ -37,15 +43,20 @@ async function markNotificationAsSeen(attractionId, userId) {
   if (!attractionId || !userId) return;
 
   try {
-    await fetch(`${apiUrl}/notifications/seen`, {
+    const response = await fetch(`${apiUrl}/notifications/seen`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({ attraction: attractionId, user: userId })
     });
-    console.log('Notification marked as seen:', subscriptionId);
+
+    if (response.ok) {
+      console.log('Notification marked as seen for user:', userId);
+    } else {
+      console.error('Failed to mark notification as seen:', response.status);
+    }
   } catch (error) {
     console.error('Error marking notification as seen:', error);
   }
-}
+};

1a676036392b5399706ba93c9580d626a62c2719
DiegoRojo99 <diego99rojo@gmail.com>
2024-10-07 21:31:34 +0100

    Added new login and register form

--- src/components/login/LoginRegister.jsx
+++ src/components/login/LoginRegister.jsx
@@ -3,16 +3,18 @@ import './LoginRegister.css';
 import { signInWithEmailAndPassword, createUserWithEmailAndPassword } from '@firebase/auth';
 import { auth } from '../../Firebase';
 import { useNavigate } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGooglePlusG, faFacebookF, faGithub, faLinkedinIn } from '@fortawesome/free-brands-svg-icons'
 
 const LoginRegister = () => {
   const [username, setUsername] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [isLogin, setIsLogin] = useState(true);
-  const apiUrl = process.env.REACT_APP_API_URL; 
+  const apiUrl = process.env.REACT_APP_API_URL;
   const navigate = useNavigate();
 
-  async function login(email, password){
+  async function login(email, password) {
     try {
       await signInWithEmailAndPassword(auth, email, password);
       navigate('/');
@@ -23,76 +25,138 @@ const LoginRegister = () => {
     }
   }
 
-  async function register(email, password){
-    await createUserWithEmailAndPassword(auth, email, password)
-      .then(async (userCredential) => {
-        const user = userCredential.user;
-        const response = await fetch(`${apiUrl}/createUser`, {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({ username, email, uid: user.uid })
-        });
-        if (!response.ok) {
-          throw new Error('Failed to add user to database');
-        }
-        else{
-          login(email, password);
-        }
-      })
-      .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        console.error('Error logging in:', errorCode, errorMessage);
-      });    
+  async function register(email, password) {
+    try {
+      const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+      const user = userCredential.user;
+
+      const response = await fetch(`${apiUrl}/createUser`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username, email, uid: user.uid }),
+      });
+
+      if (!response.ok) {
+        throw new Error('Failed to add user to database');
+      } else {
+        login(email, password);
+      }
+    } catch (error) {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.error('Error registering:', errorCode, errorMessage);
+    }
   }
 
   const handleSubmit = (e) => {
     e.preventDefault();
-
-    if(isLogin){
+    if (isLogin) {
       login(email, password);
-    }
-    else{
+    } else {
       register(email, password);
     }
   };
 
   return (
     <div className='login-page'>
-      <div className="login-register-container">
-        <h2>{isLogin ? 'Login' : 'Register'}</h2>
+    <div className={`login-container ${!isLogin ? 'active' : ''}`} id="container">
+      <div className="form-container sign-up">
         <form onSubmit={handleSubmit}>
-          { !isLogin &&
-            <input
-              type="text"
-              placeholder="Username"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-              required
-            />
-          }
-          <input
-            type="email"
-            placeholder="Email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
+          <h1>Create Account</h1>
+          <div className="social-icons">
+            <a href="#" className="icon">
+              <FontAwesomeIcon icon={faGooglePlusG} />
+            </a>
+            <a href="#" className="icon">
+              <FontAwesomeIcon icon={faFacebookF} />
+            </a>
+            <a href="#" className="icon">
+              <FontAwesomeIcon icon={faGithub} />
+            </a>
+            <a href="#" className="icon">
+              <FontAwesomeIcon icon={faLinkedinIn} />
+            </a>
+          </div>
+          <span>or use your email for registration</span>
+          <input 
+            type="text" 
+            placeholder="Name" 
+            value={username} 
+            onChange={(e) => setUsername(e.target.value)} 
+            required={!isLogin}
+          />
+          <input 
+            type="email" 
+            placeholder="Email" 
+            value={email} 
+            onChange={(e) => setEmail(e.target.value)} 
             required
           />
-          <input
-            type="password"
-            placeholder="Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
+          <input 
+            type="password" 
+            placeholder="Password" 
+            value={password} 
+            onChange={(e) => setPassword(e.target.value)} 
             required
           />
-          <button type="submit">{isLogin ? 'Login' : 'Register'}</button>
+          <button type="submit">{isLogin ? 'Sign Up' : 'Sign In'}</button>
         </form>
-        <p onClick={() => setIsLogin(!isLogin)}>
-          {isLogin ? 'Don\'t have an account? Register here' : 'Already have an account? Login here'}
-        </p>
-      </div>    
+      </div>
+
+      <div className="form-container sign-in">
+        <form onSubmit={handleSubmit}>
+          <h1>Sign In</h1>          
+          <div className="social-icons">
+            <a href="#" className="icon">
+              <FontAwesomeIcon icon={faGooglePlusG} />
+            </a>
+            <a href="#" className="icon">
+              <FontAwesomeIcon icon={faFacebookF} />
+            </a>
+            <a href="#" className="icon">
+              <FontAwesomeIcon icon={faGithub} />
+            </a>
+            <a href="#" className="icon">
+              <FontAwesomeIcon icon={faLinkedinIn} />
+            </a>
+          </div>
+          <span>or use your email for password</span>
+          <input 
+            type="email" 
+            placeholder="Email" 
+            value={email} 
+            onChange={(e) => setEmail(e.target.value)} 
+            required
+          />
+          <input 
+            type="password" 
+            placeholder="Password" 
+            value={password} 
+            onChange={(e) => setPassword(e.target.value)} 
+            required
+          />
+          <a href="#">Forgot your password?</a>
+          <button type="submit">Sign In</button>
+        </form>
+      </div>
+
+      <div className="toggle-container">
+        <div className="toggle">
+          <div className="toggle-panel toggle-left">
+            <h1>Welcome Back!</h1>
+            <p>To keep connected with us please login with your personal info</p>
+            <button className="hidden" id="login" onClick={() => setIsLogin(true)}>Sign In</button>
+          </div>
+          <div className="toggle-panel toggle-right">
+            <h1>Hello, Friend!</h1>
+            <p>Enter your personal details and start your journey with us</p>
+            <button className="hidden" id="register" onClick={() => setIsLogin(false)}>Sign Up</button>
+          </div>
+        </div>
+      </div>
+    </div>
     </div>
   );
 };

abe203a156ce38fd2c32940b1fc82b5cb60ace6f
DiegoRojo99 <diego99rojo@gmail.com>
2024-10-17 13:49:03 +0100

    Added icons for mobile nav dropdown

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,14 +1,26 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { faBars, faGear } from '@fortawesome/free-solid-svg-icons';
 import './Nav.css';
+import { faCompass, faStar, faUser } from '@fortawesome/free-regular-svg-icons';
 
 function Nav() {
   const [openMenu, setOpenMenu] = useState(false);
 
   function handleLinkClick(){
     setOpenMenu(false);
+  }
+
+  function NavItem({link, name, icon}){
+    return (
+      <Link to={link} onClick={handleLinkClick}>
+        {openMenu ? <FontAwesomeIcon icon={icon} className='nav-icon' /> : <></>}
+        <span>
+          {name}
+        </span>
+      </Link>
+    )
   }
 
   return (
@@ -18,13 +30,14 @@ function Nav() {
       </a>
       <Link className='nav-logo' to='/'  onClick={handleLinkClick}>Park Hopper</Link>
       <div className={openMenu ? "menu-items open" : "menu-items"}>
-        <Link to="/explore" onClick={handleLinkClick}>Explore</Link>
-        <Link to="/favs" onClick={handleLinkClick}>Favs</Link>
-        {/* <Link to="/settings" onClick={handleLinkClick}>Settings</Link> */}
-        <Link to="/login" onClick={handleLinkClick}>Login/Logout</Link>     
+        <NavItem link="/explore" name={"Explore"} icon={faCompass} />
+        <NavItem link="/favs" name={"Favorites"} icon={faStar} />
+        {/* <NavItem link="/settings" name={"Settings"} icon={faGear} /> */}
+        <NavItem link="/login" name={"Login/Logout"} icon={faUser} /> 
       </div>
     </div>
   );
 }
+
 
 export default Nav;

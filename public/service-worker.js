a96066846e03fbfbadb5e7a3e7645e05cc2b5930
DiegoRojo99 <diego99rojo@gmail.com>
2024-12-11 21:06:34 +0000

    Updated default body in notification

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,11 +1,10 @@
 import { sendLogToServer } from "../src/functions/logs";
-const apiUrl = process.env.REACT_APP_API_URL;
 
 self.addEventListener('push', function(event) {
   const data = event.data.json();
-  const title = data.title || 'Default Title';
+  const title = data.title || 'Park Hopper Alert';
   const options = {
-    body: data.body || 'Default body',
+    body: data.body || 'Your attraction has decreased its waiting time',
     icon: data.icon || '/park-hopper.jpeg',
     badge: data.badge || '/park-hopper.jpeg',
     data: {
@@ -21,7 +20,6 @@ self.addEventListener('notificationclick', function(event) {
   event.notification.close();
   if (attractionId) {
     sendLogToServer(`Notification clicked. Attraction ID: ${attractionId}`);
-    // clients.openWindow("https://parkhopper.live/");
   } else {
     sendLogToServer('Missing attractionId on click');
   }
@@ -30,32 +28,8 @@ self.addEventListener('notificationclick', function(event) {
 self.addEventListener('notificationclose', function(event) {
   const attractionId = event?.notification?.data?.attractionId;
   if (attractionId) {
-    // markNotificationAsSeen(attractionId, userId);
     sendLogToServer('Notification close triggered');
   } else {
     sendLogToServer('Missing attractionId on close');
   }
 });
-
-// Function to mark the notification as seen
-// async function markNotificationAsSeen(attractionId, userId) {
-//   if (!attractionId || !userId) return;
-
-//   try {
-//     const response = await fetch(`${apiUrl}/notifications/seen`, {
-//       method: 'POST',
-//       headers: {
-//         'Content-Type': 'application/json',
-//       },
-//       body: JSON.stringify({ attraction: attractionId, user: userId })
-//     });
-
-//     if (response.ok) {
-//       sendLogToServer(`Notification marked as seen. Attraction ID: ${attractionId}, User ID: ${userId}`);
-//     } else {
-//       sendLogToServer(`Failed to mark notification as seen: ${response.status}`);
-//     }
-//   } catch (error) {
-//     sendLogToServer(`Failed to mark notification as seen: ${error}`);
-//   }
-// };

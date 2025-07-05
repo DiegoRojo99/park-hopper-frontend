60c876a5f5f45f303386c02413ac01640d0422c9
DiegoRojo99 <diego99rojo@gmail.com>
2025-07-05 18:16:37 +0200

    Added color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class', 
   content: [    
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        dark: {
+          'text': '#eff4f6',
+          'background': '#04151a',
+          'primary': '#86cadf',
+          'secondary': '#16718d',
+          'accent': '#19bef0',
+        },
+        light: {
+          'text': '#1a202c',
+          'background': '#f7fafc',
+          'primary': '#2b6cb0',
+          'secondary': '#2c5282',
+          'accent': '#63b3ed',
+        },
+      },
+      fontFamily: {
+        sans: ['Poppins', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }

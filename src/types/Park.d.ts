a9f5d81fb7d6d7aabefcfc6da8dac6da56db6607
DiegoRojo99 <diego99rojo@gmail.com>
2025-08-30 15:59:17 +0200

    Added Park type

--- src/types/Park.d.ts
+++ src/types/Park.d.ts
@@ -1,8 +1,33 @@
 import { Destination } from "./db";
 import { WikimediaImage } from "./WikimediaImage";
 
+export type Park = {
+  id: string;
+  name: string;
+  slug: string;
+  destinationId: string;
+  latitude: number;
+  longitude: number;
+  timezone: string;
+  externalId: string;
+  logoImageId: string;
+  mainImageId: string;
+};
+
 export type ParkWithDetails = Park & {
   logoImage: WikimediaImage[];
   mainImage: WikimediaImage[];
   destination: Destination;
+};
+
+export type ParkWithChildren = ParkWithDetails & {
+  attractions: Attraction[];
+  shows: Show[];
+  restaurants: Restaurant[];
+};
+
+export type ParkWithChildren = ParkWithDetails & {
+  attractions: Attraction[];
+  shows: Show[];
+  restaurants: Restaurant[];
 };
